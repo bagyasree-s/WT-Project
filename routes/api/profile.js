ad11697fbd7faa117dd80e241add9e5bea03a4c3
Raghu Ganapathy <raghu.1999@gmail.com>
2020-04-05 17:02:09 +0530

    add experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -173,4 +173,49 @@ router.delete('/', auth, async (req, res) => {
         res.status(500).send('Server Error');
     }
 });
+// @route  PUT api/profile/experience
+// @desc   Delete profile, user and post
+// @access Private
+
+router.put('/experience', [auth, [
+    check('title', "Title is Required").not().isEmpty(),
+    check('company', "Company is Required").not().isEmpty(),
+    check('from', "From date is Required").not().isEmpty()
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array()
+        });
+    }
+    const {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        desription
+    } = req.body;
+    const newExp = {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        desription
+    }
+    try {
+        const profile = await Profile.findOne({
+            user: req.user.id
+        });
+        profile.experience.unshift(newExp);
+        await profile.save();
+        res.json(profile)
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 module.exports = router;
