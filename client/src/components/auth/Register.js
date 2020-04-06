b4eb74656ca5b782e3938abc97f29ee8b4024471
Raghu Ganapathy <raghu.1999@gmail.com>
2020-04-06 14:56:26 +0530

    Alerts/Redux is setup, 7 Sections completed

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,11 @@
 import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types'
 
-const Register = () => {
+
+const Register = ({ setAlert }) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -15,7 +19,7 @@ const Register = () => {
     const onSubmit = async e => {
         e.preventDefault();
         if (password !== password2) {
-            console.log('Passwords dont match');
+            setAlert('Passwords do not match.', 'danger');
         }
         else {
             console.log("success");
@@ -31,8 +35,8 @@ const Register = () => {
                 </div>
                 <div className="form-group">
                     <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} required />
-                    <small className="form-text">This site uses Gravatar so if you want a profile image, use a
-          Gravatar email</small>
+                    <small className="form-text">Our site uses Gravatar so if you want a profile image, use a
+          Gravatar email.</small>
                 </div>
                 <div className="form-group">
                     <input type="password" placeholder="Password" name="password" value={password} onChange={e => onChange(e)} required minLength="6" />
@@ -48,5 +52,7 @@ const Register = () => {
         </Fragment>
     );
 };
-
-export default Register;
+Register.propTypes = {
+    setAlert: PropTypes.func.isRequired
+}
+export default connect(null, { setAlert })(Register);
