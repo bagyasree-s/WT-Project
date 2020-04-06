ebad547d8b589265e499b94ab9a69cfd013d175f
Raghu Ganapathy <raghu.1999@gmail.com>
2020-04-06 16:58:07 +0530

    proxy error

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, {
-  Fragment
+  Fragment, useEffect
 } from 'react';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -10,26 +10,38 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // REDUX
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
 
 import './App.css';
 
 
-const App = () =>
-  <Provider store={store}>
-    <Router>
-      <Fragment >
-        <Navbar />
-        <Route exact path='/' component={Landing} />
-        <section className="container">
-          <Alert />
-          <Switch>
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
-  </Provider>
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment >
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <section className="container">
+            <Alert />
+            <Switch>
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
 export default App;
