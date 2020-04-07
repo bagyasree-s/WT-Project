05ea4f62cecd4f540ba2048d302cfb09b957ac36
Raghu Ganapathy <raghu.1999@gmail.com>
2020-04-07 15:17:26 +0530

    Login/Logout works. All bugs till here are fixed

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import {
-    REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL
+    REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOG_OUT
 } from './types';
 import { setAlert } from './alert';
 
@@ -75,3 +75,9 @@ export const login = (email, password) => async dispatch => {
         })
     }
 };
+
+// Logout/Clear Profile
+
+export const logout = () => dispatch => {
+    dispatch({ type: LOG_OUT });
+}
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,4 +1,4 @@
-import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL } from '../actions/types';
+import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOG_OUT } from '../actions/types';
 const initialState = {
     token: localStorage.getItem('token'),
     isAuthenticated: null,
@@ -49,6 +49,14 @@ export default function (state = initialState, action) {
                 loading: false
             }
         case LOGIN_FAIL:
+            localStorage.removeItem('token');
+            return {
+                ...state,
+                token: null,
+                isAuthenticated: false,
+                loading: false
+            }
+        case LOG_OUT:
             localStorage.removeItem('token');
             return {
                 ...state,
