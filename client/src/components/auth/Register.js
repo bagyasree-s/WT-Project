1f72ff2dc0637eeed74788d0addfdf01d30d443e
Raghu Ganapathy <raghu.1999@gmail.com>
2020-04-07 14:53:09 +0530

    Allbugs fixed till user can login and be redirected to dashboard

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -3,8 +3,7 @@ import { connect } from 'react-redux';
 import { Link, Redirect } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
 import { register } from '../../actions/auth';
-import PropTypes from 'prop-types'
-
+import PropTypes from 'prop-types';
 
 const Register = ({ setAlert, register, isAuthenticated }) => {
     const [formData, setFormData] = useState({
@@ -16,61 +15,89 @@ const Register = ({ setAlert, register, isAuthenticated }) => {
 
     const { name, email, password, password2 } = formData;
 
-    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+    const onChange = e =>
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+
     const onSubmit = async e => {
         e.preventDefault();
         if (password !== password2) {
-            setAlert('Passwords do not match.', 'danger');
-        }
-        else {
-            // console.log("success");
-            register({ name, email, password })
+            setAlert('Passwords do not match', 'danger');
+        } else {
+            register({ name, email, password });
         }
     };
+
     if (isAuthenticated) {
-        return <Redirect to='dashboard' />;
+        return <Redirect to='/dashboard' />;
     }
+
     return (
         <Fragment>
-            <h1 className="large text-primary">Sign Up</h1>
-            <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
-            <form className="form" onSubmit={e => onSubmit(e)}>
-                <div className="form-group">
-                    <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)}
-                        required
+            <h1 className='large text-primary'>Sign Up</h1>
+            <p className='lead'>
+                <i className='fas fa-user' /> Create Your Account
+      </p>
+            <form className='form' onSubmit={e => onSubmit(e)}>
+                <div className='form-group'>
+                    <input
+                        type='text'
+                        placeholder='Name'
+                        name='name'
+                        value={name}
+                        onChange={e => onChange(e)}
                     />
                 </div>
-                <div className="form-group">
-                    <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)}
-                        required
+                <div className='form-group'>
+                    <input
+                        type='email'
+                        placeholder='Email Address'
+                        name='email'
+                        value={email}
+                        onChange={e => onChange(e)}
                     />
-                    <small className="form-text">Our site uses Gravatar so if you want a profile image, use a
-          Gravatar email.</small>
+                    <small className='form-text'>
+                        This site uses Gravatar so if you want a profile image, use a
+                        Gravatar email
+          </small>
                 </div>
-                <div className="form-group">
-                    <input type="password" placeholder="Password" name="password" value={password} onChange={e => onChange(e)}
-                        required minLength="6"
+                <div className='form-group'>
+                    <input
+                        type='password'
+                        placeholder='Password'
+                        name='password'
+                        value={password}
+                        onChange={e => onChange(e)}
                     />
                 </div>
-                <div className="form-group">
-                    <input type="password" placeholder="Confirm Password" name="password2" value={password2} onChange={e => onChange(e)}
-                        required minLength="6"
+                <div className='form-group'>
+                    <input
+                        type='password'
+                        placeholder='Confirm Password'
+                        name='password2'
+                        value={password2}
+                        onChange={e => onChange(e)}
                     />
                 </div>
-                <input type="submit" className="btn btn-primary" value="Register" />
+                <input type='submit' className='btn btn-primary' value='Register' />
             </form>
-            <p className="my-1">
-                Already have an account? <Link to="/login">Sign In</Link>
+            <p className='my-1'>
+                Already have an account? <Link to='/login'>Sign In</Link>
             </p>
         </Fragment>
     );
 };
+
 Register.propTypes = {
     setAlert: PropTypes.func.isRequired,
     register: PropTypes.func.isRequired,
     isAuthenticated: PropTypes.bool
-}
+};
+
 const mapStateToProps = state => ({
     isAuthenticated: state.auth.isAuthenticated
-})
-export default connect(null, { setAlert, register, mapStateToProps })(Register);
+});
+
+export default connect(
+    mapStateToProps,
+    { setAlert, register }
+)(Register);
