7a55b7293a3e1db88d1cc917dee1e092fabd6d66
Raghu Ganapathy <raghu.1999@gmail.com>
2020-04-14 17:26:37 +0530

    FOR PRODUCTION

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const connectDB = require('./config/db');
 
+const path = require('path');
 const app = express();
 // connect DB
 
@@ -13,14 +14,19 @@ app.use(express.json({
 
 
 
-
-app.get('/', (req, res) => res.send("API running"));
-
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+// Serve assests for production
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    });
+}
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
